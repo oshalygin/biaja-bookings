bb9ec5bdfd92f7f2f6561b864edae162767ee65c
Oleg Shalygin <oshalygin@gmail.com>
2017-10-08 21:31:12 -0700

    Add client styles
    - This includes fonts and colors, both
      in js and css format

---
+++ client/styles/colors.js
@@ -0,0 +1,43 @@
+export default {
+  brand: {
+    red: '#ED3620',
+    blue: '#0283EA',
+    turquoise: '#00A89A',
+    orange: '#FF8F00',
+    green: '#3FA142',
+    purple: '#9209AE',
+    violet: '#3E4EB8',
+    navy: '#0048C7',
+  },
+  neutral: {
+    white: '#FFF',
+    mist: '#FAFAFA',
+    fog: '#F5F5F5',
+    typhoon: '#EEE',
+    ash: '#DDD',
+    silver: '#CCC',
+    gray: '#999',
+    dim: '#555',
+    dark: '#333',
+    black: '#000',
+  },
+  opacity: {
+    /* eslint-disable camelcase */
+    black0_8: 'rgba(0, 0, 0, 0.8)', 
+    black0_6: 'rgba(0, 0, 0, 0.6)', 
+    black0_4: 'rgba(0, 0, 0, 0.4)',
+    black0_2: 'rgba(0, 0, 0, 0.2)',
+    white0_8: 'rgba(255, 255, 255, 0.8)',
+    white0_6: 'rgba(255, 255, 255, 0.6)', 
+    white0_4: 'rgba(255, 255, 255, 0.4)',
+    white0_2: 'rgba(255, 255, 255, 0.2)',
+    /* eslint-enable camelcase */
+  },
+  utility: {
+    highlight: 'rgba(0,0,0,0.05)',
+    link: '#0283EA',
+    positive: '#3FA142',
+    negative: '#ED3620',
+    boxShadow: 'rgba(0,0,0,0.3)',
+  },
+};
---
+++ client/styles/fonts.js
@@ -0,0 +1,8 @@
+export default {
+  xxl: '1.25rem', /* 20px */
+  xl: '1.125rem', /* 18px */
+  lg: '1rem', /* 16px */
+  md: '0.875rem', /* 14px */
+  sm: '0.75rem', /* 12px */
+  xs: '0.625rem', /* 10px */
+};
