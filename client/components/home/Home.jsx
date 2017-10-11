b2508b545cb1b12166c95880aa411a01fe35840a
Oleg Shalygin <oshalygin@gmail.com>
2017-10-10 18:34:16 -0700

    Add Home Page

--- client/components/home/Home.jsx
+++ client/components/home/Home.jsx
@@ -1,5 +1,46 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import muiTheme from '../../utilities/muiTheme';
+import { RaisedButton } from 'material-ui';
 
-const Home = () => <div>Landing Page</div>;
+import history from '../../utilities/history';
+
+const navigateToDashboard = () => {
+  history.push('/dashboard');
+};
+
+const navigateToRegistration = () => {
+  history.push('/registration');
+};
+
+import './home.css';
+
+const Home = () => {
+  return (
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <div styleName="main-content">
+        <div styleName="content-box-container">
+          <div styleName="text-content">BIAJA Bookings</div>
+          <div styleName="button-control">
+            <RaisedButton
+              onClick={navigateToDashboard}
+              fullWidth
+              primary
+              label="go to dashboard"
+            />
+          </div>
+          <div styleName="button-control">
+            <RaisedButton
+              onClick={navigateToRegistration}
+              fullWidth
+              secondary
+              label="register"
+            />
+          </div>
+        </div>
+      </div>
+    </MuiThemeProvider>
+  );
+};
 
 export default Home;
