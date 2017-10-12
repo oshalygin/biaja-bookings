ec2f65660782346b8beef708a11434e1283bd029
Oleg Shalygin <oshalygin@gmail.com>
2017-10-12 08:36:21 -0700

    Home page cleanup

--- client/components/dashboard/LeftNav.jsx
+++ client/components/dashboard/LeftNav.jsx
@@ -8,13 +8,11 @@ const ListView = () =>
   <div styleName="left-nav-container">
     <List style={{ margin: 0, padding: 0 }}>
       <ListItem
-        // primaryText="Events"
         leftIcon={<FontIcon className="material-icons">events</FontIcon>}
       >
         <span styleName="list-item-text">Events</span>
       </ListItem>
       <ListItem
-        // primaryText="Artists"
         leftIcon={<FontIcon className="material-icons">person</FontIcon>}
       >
         <span styleName="list-item-text">Artists</span>
