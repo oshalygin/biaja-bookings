e07dab5024a443d26c02e9335c7c4407d0c618f7
Oleg Shalygin <oshalygin@gmail.com>
2017-10-11 17:55:13 -0700

    Add routing reducer

--- client/reducers/rootReducer.js
+++ client/reducers/rootReducer.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import initialState from './initialState';
 import events from './eventReducer';
 
 const rootReducer = combineReducers({
+  routing: routerReducer,
   events,
 });
 
