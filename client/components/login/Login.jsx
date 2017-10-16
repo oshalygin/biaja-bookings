3f15a90d7781ab89b6bd62fd3de6948f62474a1a
Oleg Shalygin <oshalygin@gmail.com>
2017-10-15 23:35:43 -0700

    Update Login Page

--- client/components/login/Login.jsx
+++ client/components/login/Login.jsx
@@ -1,42 +1,114 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actionCreators from '../../actions/userActions';
+import history from '../../utilities/history';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import muiTheme from '../../utilities/muiTheme';
-
-import history from '../../utilities/history';
 
 import './login.css';
 import Snackbar from '../common/Snackbar';
 import Spinner from '../common/Spinner';
 import LoginForm from './LoginForm';
+import validation from '../../utilities/validation';
+
+const propTypes = {
+  history: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired,
+  userActions: PropTypes.object.isRequired,
+  notificationMessage: PropTypes.string,
+};
 
 class Login extends React.Component {
   state = {
-    username: '',
-    password: '',
-    errorText: {
-      username: false,
-      password: false,
-    },
+    user: {},
+    loading: false,
+    formErrors: {},
+    notification: false,
+    notificationMessage: '',
   };
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.notificationMessage) {
+      const { notificationMessage } = nextProps;
+
+      this.setState({
+        notification: true,
+        formErrors: { ...this.state.formErrors, server: notificationMessage },
+        notificationMessage,
+      });
+    }
+  }
 
   navigateToRegistration = () => {
-    history.push('/register');
+    history.push('/registration');
   };
 
-  onChange = () => {};
+  validateForm = () => {
+    const { user, formErrors } = this.state;
+    const { email, password } = user;
 
-  login = () => {};
+    let isValid = true;
+    let errors = { ...formErrors };
+
+    if (!password) {
+      errors = { ...errors, password: true };
+      isValid = false;
+    } else {
+      errors = { ...errors, password: false };
+    }
+
+    if (!validation.validateEmail(email)) {
+      errors = { ...errors, email: true };
+      isValid = false;
+    } else {
+      errors = { ...errors, email: false };
+    }
+
+    this.setState({ formErrors: errors });
+    return isValid;
+  };
+
+  closeNotification = () => {
+    this.setState({ notification: false });
+  };
+
+  onChange = event => {
+    const property = event.target.name;
+    const { user } = this.state;
+    user[property] = event.target.value;
+  };
+
+  submit = event => {
+    event.preventDefault();
+    const { user } = this.state;
+    const { userActions } = this.props;
+
+    if (this.validateForm()) {
+      userActions.userLogin(user);
+    }
+  };
 
   render() {
-    const formErrors = {};
-    const loading = false;
+    const { loading } = this.props;
+    const { formErrors } = this.state;
+
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div>
           <div className="middle-box text-center loginscreen animated fadeInDown">
             <div>
               <div>
-                <h1 className="logo-name" style={{ fontSize: '10em', marginBottom: '0.25em' }}>BIAJI</h1>
+                <h1
+                  className="logo-name"
+                  style={{ fontSize: '10em', marginBottom: '0.25em' }}
+                >
+                  BIAJI
+                </h1>
               </div>
               <h3>Welcome back to the Artist Dashboard</h3>
               <p>Login to get started</p>
@@ -49,9 +121,7 @@ class Login extends React.Component {
               />
               <Spinner hidden={!loading} />
               <p className="m-t">
-                <small>
-                 BIAJI is a registered trademark of BIAJI, LLC.
-                </small>
+                <small>BIAJI is a registered trademark of BIAJI, LLC.</small>
               </p>
             </div>
           </div>
@@ -68,4 +138,16 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+Login.propTypes = propTypes;
+
+const mapStateToProps = state => ({
+  user: state.user,
+  loading: state.loading.loadingUserLogin,
+  notificationMessage: state.notification,
+});
+
+const mapDispatchToProps = dispatch => ({
+  userActions: bindActionCreators(actionCreators, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
