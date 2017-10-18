0b66acbe4ca99351e6cf9185d610796df00fd21b
Oleg Shalygin <oshalygin@gmail.com>
2017-10-18 00:40:17 -0700

    Update cron event controller

--- server/controllers/cronEventsController.js
+++ server/controllers/cronEventsController.js
@@ -1,7 +1,7 @@
 import eventService from '../services/eventService';
 
 export async function get(request, response) {
-  const events = await eventService.hydrateEvents();
+  const events = await eventService.hydrateArtistEventsIteratively();
   return response.status(200).json(events);
 }
 
