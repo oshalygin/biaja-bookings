423830a6750c310be861bf3f7e75e89a9f976568
Oleg Shalygin <oshalygin@gmail.com>
2017-10-18 21:44:00 -0700

    Clean up event service
    - The event service has been cleaned up to remove erroneous
      functions and dead code

--- server/services/eventService.js
+++ server/services/eventService.js
@@ -1,20 +1,10 @@
 /* eslint-disable quotes */
-import axios from 'axios';
-import cheerio from 'cheerio';
 import Crawler from 'crawler';
 
 import artists from '../dataAccess/artists';
 import secondaryArtists from '../dataAccess/secondaryArtists';
-import db from '../dataAccess/database';
 import yelpService from './yelpService';
-import dateUtilities from '../utilities/dateUtilities';
 import eventsDAL from '../dataAccess/eventsDAL';
-
-const updatedQueryBasedOnHistoricalFlag = (query, historical) => {
-  return historical
-    ? query.where('dateMillis', '<', dateUtilities.historicalCutoffDate())
-    : query.where('dateMillis', '>=', dateUtilities.historicalCutoffDate());
-};
 
 const crawler = new Crawler({
   rateLimit: 3000,
@@ -29,7 +19,7 @@ const hydrateArtistEvent = artist => {
 
         callback(error, res, done) {
           if (error) {
-            console.log(error);
+            console.log(error); //eslint-disable-line no-console
             reject(error);
           } else {
             let events = [];
@@ -79,12 +69,6 @@ async function hydrateArtistEventsIteratively() {
     hydrateArtistEvent(artist),
   );
   await Promise.all(performances);
-
-  // const events = collectionOfEventPromises.reduce((previous, next) => {
-  //   return [...previous, ...next];
-  // }, []);
-
-  // await yelpService.hydrateEventsWithYelpData(events);
 }
 
 async function hydrateArtistEvents() {
@@ -97,80 +81,6 @@ async function hydrateArtistEvents() {
 
   await yelpService.hydrateEventsWithYelpData(events);
 }
-
-async function scrapeArtistData(artist) {
-  let events = [];
-  const { data } = await axios.get(artist.url);
-
-  const $ = cheerio.load(data); //eslint-disable-line id-length
-  const locations = $('tr.microformats');
-
-  //eslint-disable-next-line array-callback-return
-  locations.map((index, element) => {
-    const date = $(element).find('meta').attr('content');
-    const venue = $(element).find('.venue span[itemprop="name"]').text();
-    const city = $(element)
-      .find('.location span[itemprop="addressLocality"]')
-      .text();
-    const state = $(element)
-      .find('.location span[itemprop="addressRegion"]')
-      .text();
-
-    events = [
-      ...events,
-      {
-        dateAdded: new Date(),
-        artist: artist.name,
-        date,
-        venue,
-        city,
-        state,
-        country: state ? 'United States' : 'Overseas',
-      },
-    ];
-  });
-
-  return events;
-}
-
-async function scrapeTourData() {
-  const performances = artists.map(artist => scrapeArtistData(artist));
-  const collectionOfTours = await Promise.all(performances);
-
-  const tourData = collectionOfTours.reduce((previous, next) => {
-    return [...previous, ...next];
-  }, []);
-
-  return tourData;
-}
-
-// async function hydrateEvent(event) {
-//   const eventCollection = db.collection('events');
-
-//   const snapshot = await eventCollection
-//     .where('artist', '==', event.artist)
-//     .where('date', '==', event.date)
-//     .get();
-
-//   if (snapshot.size) {
-//     return true;
-//   }
-
-//   await eventCollection.add({
-//     ...event,
-//     eventType: 'Ticketed Concerts',
-//     dateMillis: new Date(event.date).valueOf(),
-//   });
-//   return true;
-// }
-
-// async function hydrateEvents() {
-//   const events = await scrapeTourData();
-
-//   const updatedEvents = await yelpService.hydrateEventsWithYelpData(events);
-//   const hydratedEvents = updatedEvents.map(event => hydrateEvent(event));
-//   await Promise.all(hydratedEvents);
-// }
 
 async function getAllEvents(historical) {
   const data = historical
@@ -205,7 +115,6 @@ async function getEventsOverseas(city, historical) {
 }
 
 const eventService = {
-  // hydrateEvents,
   hydrateArtistEvents,
   getAllEvents,
   getEventsInUnitedStates,
